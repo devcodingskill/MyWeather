68425fdabd92704ce59be852ca40922d804a59ff
Yutthana Tantinimitchok <tan@inspectrealestate.com.au>
2023-07-05 14:33:33 +1000

    Add image and dashboard detail

--- ViewModels/BaseViewModel.cs
+++ ViewModels/BaseViewModel.cs
@@ -1,12 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
 
 namespace MyWeather.ViewModels
 {
-    public class BaseViewModel
+    public partial class BaseViewModel : ObservableObject
     {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        private bool isBusy;
+        
+        [ObservableProperty]
+        private string title;
+        public bool IsNotBusy => !IsBusy;
     }
 }
